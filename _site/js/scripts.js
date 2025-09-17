257345be6a7ce01168f6dc612bb48e04c1e6007e
RaphBi <2raphael.bigey@gmail.com>
2025-09-17 20:52:51 +0200

    ahg

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded', event => {
     };
 });
 window.addEventListener('DOMContentLoaded', event => {
-    // Activate Bootstrap scrollspy on the main nav element
+    
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
