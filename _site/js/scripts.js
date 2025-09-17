da84f115afb7a0e91fb4a88095f31648091271b2
RaphBi <2raphael.bigey@gmail.com>
2025-09-17 18:04:04 +0200

    Grosses modif site

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -17,25 +17,6 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     };
 
-    // Collapse responsive navb/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
@@ -82,38 +63,3 @@ window.addEventListener('DOMContentLoaded', event => {
         sessionStorage.removeItem('scrollToSection');
     }
 });
-ar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-    // Ajoute le code pour le défilement fluide sans # dans l'URL
-    document.querySelectorAll('.smooth-scroll').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault(); // Empêche le comportement par défaut du lien
-
-            // Récupère l'identifiant de la section cible
-            const targetId = this.getAttribute('data-target');
-            const targetElement = document.getElementById(targetId);
-
-            // Fait défiler la page jusqu'à l'élément cible
-            if (targetElement) {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth' // Défilement fluide
-                });
-            }
-        });
-    });
-});
-
-
-
-
